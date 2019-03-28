2debe5dff82358b66d9a9af96c99164b988aa228
kawadeomkar <kawade.omkar21@gmail.com>
2019-03-28 21:05:39 +0900

    refactored code, added all exercises

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
 # Omkar Kawade
 # preproceess data/data.rtf 
 
+FILE_NAME = 'data/data.txt'
+
 # takes in a filename and returns list of individual workout sessions
 def dataSrc(filename):
 	data = ''
@@ -11,26 +13,60 @@ def dataSrc(filename):
 	
 
 if __name__ == '__main__':
+	# filename used
+	global FILE_NAME
+
 	# dictionary containing all workout statistics
 	workouts = {}
-	filename = 'data/data.txt'
-	data = dataSrc(filename)
+	# helper variables
+	previous_muscle = ''
+	previous_date = ''
 
+	data = dataSrc(FILE_NAME)
+	
+	# loop through each split workout	
 	for day in data:
 		day = day.split('\n')
+		# check if the row is a new workout
 		if day[0] == '' and len(day) > 3:
 			newMuscleGroup = day[1].split(' ')
+			exercise_name = day[2]
 			date, muscle = newMuscleGroup[0], newMuscleGroup[1]
+			
+			# find max weight in sub list 
 			max_weight = 0.0
 			for exercise in day[3:len(day)]:
 				set = ((exercise.split(' '))[1]).split('x')
 				reps, weight = int(set[0]), float(set[1])
 				if weight > max_weight and reps >= 3:
 					max_weight = weight
+
+			# create new dictionary within workouts dictionary if muscle DNE
 			if muscle not in workouts.keys():
 				workouts[muscle] = {}
-			workouts[muscle][date] = max_weight
-
+			# create new dictionary within workouts[muscle] if exercise name DNE
+			if exercise_name not in workouts[muscle].keys():
+				workouts[muscle][exercise_name] = {}
+			# set the date equal to max_weight of set
+			workouts[muscle][exercise_name][date] = max_weight
+			previous_muscle, previous_date = muscle, date
+		# continuing workout exercisees
+		elif len(day) > 1:
+			exercise_name = day[0]
+			
+			# find max weight in sub list
+			max_weight = 0.0
+			for exercise in day[3:len(day)]:
+				set = ((exercise.split(' '))[1]).split('x')
+				reps, weight = int(set[0]), float(set[1])
+				if weight > max_weight and reps >= 3:
+					max_weight = weight
+			
+			# create new dictionary within workouts[muscle] if exercise name DNE
+			if exercise_name not in workouts[previous_muscle].keys():
+				workouts[previous_muscle][exercise_name] = {}
+			workouts[previous_muscle][exercise_name][previous_date] = max_weight
+			
 	# test results for chest
 	for day, weight in workouts['chest'].items():
 		print(day, weight)
